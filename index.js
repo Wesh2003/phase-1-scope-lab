fca42d7fd56e50151853ca312a8aea5a5c458342
Tumaini Muriithi <wendomuriithi@gmail.com>
2023-10-08 02:21:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+
+    return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+
+    var bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer() {
+
+    var bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Agnes';
+
+function changeLeastFavoriteCustomer() {
+    
+    const changeLeastFavoriteCustomer = 'Dorcas';
+}
